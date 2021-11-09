f90041ad0b9a8cccf34d0aac6bb75ee5d02f7bca
Arif Sinan Çeçen <34174457+arifsinancecen@users.noreply.github.com>
2021-11-09 11:20:09 +0300

    Create Welder.java

---
+++ Welder.java
@@ -0,0 +1,17 @@
+/*
+DoorFittingExpert interface :
+
+getDescription()
+*/
+
+class Welder {
+    getDescription() {
+        console.log('I can only fit iron doors')
+    }
+}
+
+class Carpenter {
+    getDescription() {
+        console.log('I can only fit wooden doors')
+    }
+}
