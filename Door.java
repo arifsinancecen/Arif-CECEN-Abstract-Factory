7ce55bdee4efdc4b4c78cfc64d38fe118a6e5c98
Arif Sinan Çeçen <34174457+arifsinancecen@users.noreply.github.com>
2021-11-09 11:19:41 +0300

    Create Door.java

---
+++ Door.java
@@ -0,0 +1,17 @@
+/*
+Door interface :
+
+getDescription()
+*/
+
+class WoodenDoor {
+    getDescription() {
+        console.log('I am a wooden door')
+    }
+}
+
+class IronDoor {
+    getDescription() {
+        console.log('I am an iron door')
+    }
+}
